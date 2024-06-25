82944ae2e97992c7e7c93d48b27eee2ddb2f9ac4
ambarrera <91036781+ambarrera@users.noreply.github.com>
2024-06-25 18:53:17 -0500

    Add files via upload

---
+++ DynamicForm/CV_.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms;
+
+namespace DynamicForm
+{
+    public partial class CVPage : ContentPage
+    {
+        public CVPage(
+            string nombre, string apellidos, string fechaNacimiento, string nacionalidad,
+            string aptitudes, string habilidades, string lenguajes,
+            string perfil, string experienciaLaboral, string educacion)
+        {
+            InitializeComponent();
+
+            labelNombre.Text = $"Nombre: {nombre}";
+            labelApellidos.Text = $"Apellidos: {apellidos}";
+            labelFechaNacimiento.Text = $"Fecha de Nacimiento: {fechaNacimiento}";
+            labelNacionalidad.Text = $"Nacionalidad: {nacionalidad}";
+            labelAptitudes.Text = $"Aptitudes: {aptitudes}";
+            labelHabilidades.Text = $"Habilidades: {habilidades}";
+            labelLenguajes.Text = $"Lenguajes de Programación: {lenguajes}";
+            labelPerfil.Text = $"Perfil: {perfil}";
+            labelExperienciaLaboral.Text = $"Experiencia Laboral: {experienciaLaboral}";
+            labelEducacion.Text = $"Educación: {educacion}";
+        }
+    }
+}
---
+++ DynamicForm/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace DynamicForm
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnEnviarClicked(object sender, EventArgs e)
+        {
+            var nombre = entryNombre.Text;
+            var apellidos = entryApellidos.Text;
+            var fechaNacimiento = datePickerFechaNacimiento.Date.ToString("d");
+            var nacionalidad = pickerNacionalidad.SelectedItem?.ToString();
+
+            var aptitudes = new System.Text.StringBuilder();
+            foreach (CheckBox aptitud in stackAptitudes.Children)
+            {
+                if (aptitud.IsChecked)
+                    aptitudes.AppendLine(aptitud.Content.ToString());
+            }
+
+            var habilidades = new System.Text.StringBuilder();
+            foreach (CheckBox habilidad in stackHabilidades.Children)
+            {
+                if (habilidad.IsChecked)
+                    habilidades.AppendLine(habilidad.Content.ToString());
+            }
+
+            var lenguajes = new System.Text.StringBuilder();
+            foreach (CheckBox lenguaje in stackLenguajes.Children)
+            {
+                if (lenguaje.IsChecked)
+                    lenguajes.AppendLine(lenguaje.Content.ToString());
+            }
+
+            var perfil = editorPerfil.Text;
+            var experienciaLaboral = editorExperienciaLaboral.Text;
+            var educacion = editorEducacion.Text;
+
+            await Navigation.PushAsync(new CVPage(
+                nombre, apellidos, fechaNacimiento, nacionalidad,
+                aptitudes.ToString(), habilidades.ToString(), lenguajes.ToString(),
+                perfil, experienciaLaboral, educacion));
+        }
+    }
+}
